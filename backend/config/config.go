7dbfacb859eac30a19913cc6296959d618ee565c
AntonioMartinezLopez <martinezlopez@outlook.de>
2024-07-11 09:32:13 +0200

    add check for env variables

--- backend/config/config.go
+++ backend/config/config.go
@@ -2,13 +2,23 @@ package config
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/joho/godotenv"
 )
+
+var variables = []string{"PORT"}
 
 func LoadConfig() {
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Printf("Error loading .env file")
+	}
+
+	for _, env := range variables {
+		_, envExists := os.LookupEnv(env)
+		if !envExists {
+			panic(fmt.Sprintf("Missing env variable '%s'. Aborting...\n", env))
+		}
 	}
 }
