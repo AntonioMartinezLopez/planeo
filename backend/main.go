801a169fb068091365e179e40cd4a09739a74b65
AntonioMartinezLopez <martinezlopez@outlook.de>
2024-07-11 09:09:03 +0200

    refactor and use configuration function for initialising router

--- backend/main.go
+++ backend/main.go
@@ -3,20 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"planeo/api/config"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := "Hello world"
-	fmt.Println(message)
+
+	// Load configuration
+	config.LoadConfig()
+
+	// Start server
+	port := os.Getenv("PORT")
+	fmt.Println("Starting server on Port " + port)
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(fmt.Sprintf(":%s", port), r)
 }
