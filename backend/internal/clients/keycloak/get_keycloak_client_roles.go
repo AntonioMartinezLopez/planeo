eb83d12d217959748755382e370768997f7332d4
Antonio Martinez Lopez <martinezlopez@outlook.de>
2024-10-27 17:57:00 +0100

    refactor get client roles function

--- backend/internal/clients/keycloak/get_keycloak_client_roles.go
+++ backend/internal/clients/keycloak/get_keycloak_client_roles.go
@@ -2,48 +2,23 @@ package keycloak
 
 import (
 	"fmt"
-	jsonHelper "planeo/api/pkg/json"
-	"planeo/api/pkg/logger"
 	"planeo/api/pkg/request"
 )
 
 func (kc *KeycloakAdminClient) GetKeycloakClientRoles(clientUuid string) ([]KeycloakClientRole, error) {
 
-	accessToken, err := kc.getAccessToken()
-
-	if err != nil {
-		return nil, err
+	requestParams := KeycloakRequestParams{
+		Method:  request.GET,
+		Url:     fmt.Sprintf("%s/admin/realms/%s/clients/%s/roles", kc.baseUrl, kc.realm, clientUuid),
+		Payload: nil,
 	}
-
-	headers := map[string]string{
-		"Authorization": fmt.Sprintf("Bearer %s", accessToken),
-	}
-
-	requestParams := request.HttpRequestParams{
-		Method:      request.GET,
-		URL:         fmt.Sprintf("%s/admin/realms/%s/clients/%s/roles", kc.baseUrl, kc.realm, clientUuid),
-		Headers:     headers,
-		ContentType: request.ApplicationJSON,
-	}
-
-	response, err := request.HttpRequestWithRetry(requestParams)
-
-	if err != nil {
-		return nil, err
-	}
-
-	if response.StatusCode != 200 {
-		return nil, fmt.Errorf("something went wrong: Fetching Keycloak endpoint resulted in http response %d", response.StatusCode)
-	}
-
-	defer response.Body.Close()
 
 	var keycloakClientRoles []KeycloakClientRole
-	validationError := jsonHelper.DecodeJSONAndValidate(response.Body, &keycloakClientRoles, true)
 
-	if validationError != nil {
-		logger.Error("Validation error: %s", validationError)
-		return nil, validationError
+	requestError := SendRequest(kc, requestParams, &keycloakClientRoles)
+
+	if requestError != nil {
+		return nil, requestError
 	}
 
 	return keycloakClientRoles, nil
