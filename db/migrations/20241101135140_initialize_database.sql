7765f01230be4c154a98d8be7143bdf9321f8958
Antonio Martinez Lopez <antonio.martinez.lopez@outlook.de>
2025-01-16 10:12:57 +0100

    refactor user module

--- db/migrations/20241101135140_initialize_database.sql
+++ db/migrations/20241101135140_initialize_database.sql
@@ -7,11 +7,11 @@ CREATE TABLE users (
     first_name text NOT NULL,
     last_name text NOT NULL,
     email text NOT NULL,
-    keycloak_id text NOT NULL,
+    iam_user_id text NOT NULL,
     organization text NOT NULL
 );
 
-INSERT INTO "users" ("username", "first_name", "last_name", "email", "keycloak_id", "organization") VALUES 
+INSERT INTO "users" ("username", "first_name", "last_name", "email", "iam_user_id", "organization") VALUES 
 ('admin', 'admin', 'admin', 'admin@local.de', '7c806e52-e7cc-484b-843b-1242046590dc', 'local'),
 ('planner', 'planner', 'planner', 'planner@local.de', '146b3857-090e-453d-b1e6-8cdfbb1a6dcb', 'local'),
 ('user', 'User', 'User', 'user@local.de', 'd7eddb93-254e-4482-9a53-f31a5975dd1d', 'local');
