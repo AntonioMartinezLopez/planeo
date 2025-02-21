ad7798e4efebfc3a9fa2f479556b00b4cc8d2b16
Antonio Martinez Lopez <antonio.martinez.lopez@outlook.de>
2025-02-21 18:22:27 +0100

    add e2e tests for get and post operations

--- backend/internal/test/request/request_test.go
+++ backend/internal/test/request/request_test.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 	"planeo/api/internal/resources/request"
+	"planeo/api/internal/resources/request/dto"
+	"planeo/api/internal/resources/request/models"
 	"planeo/api/internal/setup"
 	"planeo/api/internal/test/utils"
 	"planeo/api/pkg/db"
+	jsonHelper "planeo/api/pkg/json"
 	"testing"
 
 	"github.com/danielgtaylor/huma/v2/humatest"
@@ -51,6 +54,7 @@ func TestRequestIntegration(t *testing.T) {
 						t.Error(err)
 					}
 
+					assert.NotNil(t, session)
 					response := api.Get("/organizations/1/requests?pageSize=10", fmt.Sprintf("Authorization: Bearer %s", session.AccessToken))
 
 					assert.Equal(t, 200, response.Code)
@@ -58,5 +62,137 @@ func TestRequestIntegration(t *testing.T) {
 			}
 		})
 
+		t.Run("should return 403 when user is not authorized", func(t *testing.T) {
+			session, err := env.GetUserSession("user", "user")
+
+			if err != nil {
+				t.Error(err)
+			}
+			assert.NotNil(t, session)
+			response := api.Get("/organizations/1/requests?pageSize=10", fmt.Sprintf("Authorization: Bearer %s", session.AccessToken))
+
+			assert.Equal(t, 401, response.Code)
+		})
+
+		t.Run("should return 401 with missing authorization header", func(t *testing.T) {
+			response := api.Get("/organizations/1/requests?pageSize=10")
+
+			assert.Equal(t, 401, response.Code)
+		})
+
+		t.Run("should return 401 with invalid authorization header", func(t *testing.T) {
+			response := api.Get("/organizations/1/requests?pageSize=10", "Authorization: Bearer invalid")
+
+			assert.Equal(t, 401, response.Code)
+		})
+
+		t.Run("should return 403 for unknown organization to user", func(t *testing.T) {
+			session, err := env.GetUserSession("admin", "admin")
+
+			if err != nil {
+				t.Error(err)
+			}
+			assert.NotNil(t, session)
+			response := api.Get("/organizations/3525/requests?pageSize=10", fmt.Sprintf("Authorization: Bearer %s", session.AccessToken))
+
+			assert.Equal(t, 403, response.Code)
+		})
+
+		t.Run("should return all requests with pageSize 10", func(t *testing.T) {
+			session, err := env.GetUserSession("admin", "admin")
+
+			if err != nil {
+				t.Error(err)
+			}
+			assert.NotNil(t, session)
+			response := api.Get("/organizations/1/requests?pageSize=10", fmt.Sprintf("Authorization: Bearer %s", session.AccessToken))
+
+			assert.Equal(t, 200, response.Code)
+			// Add more assertions to check the response body if needed
+		})
+
+		t.Run("should return paginated requests", func(t *testing.T) {
+			session, err := env.GetUserSession("admin", "admin")
+
+			if err != nil {
+				t.Error(err)
+			}
+			assert.NotNil(t, session)
+			response := api.Get("/organizations/1/requests?pageSize=3", fmt.Sprintf("Authorization: Bearer %s", session.AccessToken))
+
+			var requests struct {
+				Requests   []models.Request
+				NextCursor int
+			}
+			jsonHelper.DecodeJSONAndValidate(response.Result().Body, &requests, true)
+			assert.Equal(t, 200, response.Code)
+			assert.Equal(t, 3, len(requests.Requests))
+
+			// Fetch the next page
+			nextPageResponse := api.Get(fmt.Sprintf("/organizations/1/requests?pageSize=3&cursor=%d", requests.NextCursor), fmt.Sprintf("Authorization: Bearer %s", session.AccessToken))
+			jsonHelper.DecodeJSONAndValidate(nextPageResponse.Result().Body, &requests, true)
+			assert.Equal(t, 200, nextPageResponse.Code)
+			assert.Equal(t, 2, len(requests.Requests))
+		})
+	})
+
+	t.Run("POST /requests ", func(t *testing.T) {
+
+		t.Run("should return 403 when user is not authorized", func(t *testing.T) {
+			session, err := env.GetUserSession("user", "user")
+
+			if err != nil {
+				t.Error(err)
+			}
+			assert.NotNil(t, session)
+			response := api.Post("/organizations/1/requests", fmt.Sprintf("Authorization: Bearer %s", session.AccessToken), dto.CreateRequestInputBody{})
+
+			assert.Equal(t, 401, response.Code)
+		})
+
+		t.Run("should return 401 with missing authorization header", func(t *testing.T) {
+			response := api.Post("/organizations/1/requests", "", dto.CreateRequestInputBody{})
+
+			assert.Equal(t, 401, response.Code)
+		})
+
+		t.Run("should return 401 with invalid authorization header", func(t *testing.T) {
+			response := api.Post("/organizations/1/requests", "Authorization: Bearer invalid", dto.CreateRequestInputBody{})
+
+			assert.Equal(t, 401, response.Code)
+		})
+
+		t.Run("should return 403 for unknown organization to user", func(t *testing.T) {
+			session, err := env.GetUserSession("admin", "admin")
+
+			if err != nil {
+				t.Error(err)
+			}
+			assert.NotNil(t, session)
+			response := api.Post("/organizations/3525/requests", fmt.Sprintf("Authorization: Bearer %s", session.AccessToken), dto.CreateRequestInputBody{})
+
+			assert.Equal(t, 403, response.Code)
+		})
+
+		t.Run("should create a request", func(t *testing.T) {
+			session, err := env.GetUserSession("admin", "admin")
+
+			if err != nil {
+				t.Error(err)
+			}
+			assert.NotNil(t, session)
+
+			body := dto.CreateRequestInputBody{
+				Text:      "Test request",
+				Name:      "Test request",
+				Email:     "test.test@test.com",
+				Address:   "123 Main St",
+				Telephone: "123-456-7890",
+				Closed:    false,
+			}
+
+			response := api.Post("/organizations/1/requests", fmt.Sprintf("Authorization: Bearer %s", session.AccessToken), body)
+			assert.Equal(t, 200, response.Code)
+		})
 	})
 }
