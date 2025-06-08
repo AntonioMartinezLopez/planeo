d67d2e23998592d063d307dc8d43e33730fb4a4c
Antonio Martinez Lopez <antonio.martinez.lopez@outlook.de>
2025-06-08 19:19:37 +0200

    remove auth endpoints from api base route

--- web/app/pages/login.vue
+++ web/app/pages/login.vue
@@ -2,7 +2,7 @@
   <div>
     <p v-if="!loggedIn">
       You are not logged in.
-      <a href="/api/auth/keycloak">Login with Keycloak</a>
+      <a href="/auth/keycloak">Login with Keycloak</a>
     </p>
   </div>
 </template>
