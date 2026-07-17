3f8e9f7238047dfbc4798836eb447d10f5a5da9d
Antonio Martinez Lopez <antonio.martinez.lopez@outlook.de>
2026-07-17 12:55:34 +0200

    style(infra): tofu fmt alignment fix

--- infra/environments/local/keycloak/main.tf
+++ infra/environments/local/keycloak/main.tf
@@ -8,9 +8,9 @@ module "realm" {
 module "org_local" {
   source = "../../../modules/keycloak-org"
 
-  realm_id         = module.realm.id
-  org_name         = "local"
-  client_secret    = var.org_client_secret
+  realm_id      = module.realm.id
+  org_name      = "local"
+  client_secret = var.org_client_secret
 }
 
 locals {
